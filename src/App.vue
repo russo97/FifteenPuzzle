f68a5873237018e7518a7fbe06bbb73b3a645044
Wanderson Silva <wandersonsilva768@gmail.com>
2020-09-18 02:45:27 -0300

    feat: estrutura estática inicial

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <main id="app">
-    works
+    <div id="content">
+      <div
+        :key="index"
+        class="cell"
+        v-for="(n, index) in 16">
+        {{ n }}
+      </div>
+    </div>
   </main>
 </template>
 
@@ -9,10 +16,35 @@
 export default {
   name: 'App',
 
+  data () {
+    return {};
+  },
+
   components: {}
 }
 </script>
 
-<style>
+<style lang="scss">
+  main#app {
+    width: 420px;
+    height: 420px;
+    display: flex;
+    position: relative;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+    box-shadow: 0 3px 6px -2px #aaa;
 
+    div#content {
+      width: calc(100% - 6px);
+      height: calc(100% - 6px);
+      display: grid;
+      grid-gap: 3px;
+      grid-template: repeat(4, 1fr) / repeat(4, 1fr);
+
+      .cell {
+        background-color: #F15E5Ebd;
+      }
+    }
+  }
 </style>
