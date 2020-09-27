4b7fdd3788a0ec644752f2e6d1285128f397f5f8
Wanderson Silva <wandersonsilva768@gmail.com>
2020-09-27 04:02:09 -0300

    feat: adicionado delay nas animações para controle de entrada e saída da tela de fim de jogo

--- src/components/WinScreen.vue
+++ src/components/WinScreen.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="winScreen" :class="{ 'winner': winner }">
       <div class="winnerBox" :class="{ 'winner': winner }">
-          amazing
+          <h1>Amazing</h1>
+
+          <p>you did it</p>
       </div>
   </div>
 </template>
@@ -27,9 +29,11 @@ export default {
         height: 100%;
         position: absolute;
         @extend %flex-center-center;
+        transition: all 0ms ease 500ms;
 
         &.winner {
             left: 0px;
+            transition: all 0ms ease 0ms;
         }
 
         .winnerBox {
@@ -37,13 +41,31 @@ export default {
             width: 75%;
             height: 50%;
             margin-top: 40px;
-            background: white;
-            box-shadow: 0 2px 5px 5px #fff;
-            transition: all 500ms ease-in-out 300ms;
+            flex-direction: column;
+            @include border-radius(3px);
+            box-shadow: 0 2px 4px #F15E5E;
+            @extend %flex-center-space-evenly;
+            background: rgba(255, 255, 255, 0.811);
+            transition: all 500ms ease-in-out 0ms;
 
             &.winner {
                 opacity: 1;
                 margin-top: -20px;
+                transition: all 500ms ease-in-out 0ms;
+            }
+
+            h1 {
+                color: #F15E5E;
+                font-size: 2.4rem;
+                font-family: Pacifico;
+                text-transform: uppercase;
+                text-shadow: 0 2px 4px #F15E5E;
+            }
+
+            p {
+                color: #F15E5E;
+                font-size: 1.4rem;
+                font-family: "Overpass";
             }
         }
     }
